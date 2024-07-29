4f10651a680d72234fd6dcb601b00250b795e65d
Никита Баландин <nik.balandin.01@mail.ru>
2024-07-30 00:23:59 +0500

    Создан модуль classes. В модуле созданы классы Category и Product

---
+++ Src/classes.py
@@ -0,0 +1,23 @@
+class Category:
+    name: str
+    description: str
+    goods: list
+    total_of_category = 0
+    total_of_products = 0
+
+    def __init__(self, name, description, goods):
+        self.name = name
+        self.description = description
+        self.goods = goods
+
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity_in_stock: int
+
+    def __init__(self, name, description, price, quantity_in_stock):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity_in_stock = quantity_in_stock
